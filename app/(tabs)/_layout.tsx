80454b2980952b31e0a915c7eaff12ccdf396f9b
Akshajk3 <78132834+Akshajk3@users.noreply.github.com>
2025-03-29 15:40:14 -0700

    restored navbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,52 +1,56 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons"
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+            tabBarActiveTintColor: "#ffd33d",
+            headerStyle: {
+                backgroundColor: "#25292e",
+            },
+            headerShadowVisible: false,
+            headerTintColor: "#fff",
+            tabBarStyle: {
+                backgroundColor: "#25292e",
           },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
-      />
-      <Tabs.Screen
-        name="search"
+    >
+    <Tabs.Screen name="index"
         options={{
-          title: 'Search',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="favorites"
+        headerTitle: "Home",
+        tabBarLabel: "Home",
+        tabBarIcon: ({focused, color}) => <Ionicons 
+            name={focused ? "home-sharp" : "home-outline"}
+            size={24}
+            color={color}
+        />
+      }}/>
+      <Tabs.Screen name="search" 
         options={{
-          title: 'Favorites',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="heart.fill" color={color} />,
-        }}
-      />
+        headerTitle: "Search",
+        tabBarLabel: "Search",
+        tabBarIcon: ({focused, color}) => <Ionicons
+            name={focused ? "search" : "search-outline"}
+            size={24}
+            color={color}
+        />
+      }}/>
+
+    <Tabs.Screen name="favorites" 
+        options={{
+        headerTitle: "Favorites",
+        tabBarLabel: "Favorites",
+        tabBarIcon: ({focused, color}) => <Ionicons
+            name={focused ? "star" : "star-outline"}
+            size={24}
+            color={color}
+        />
+      }}/>
+
+      <Tabs.Screen name="+not-found" 
+      options={{
+      }}/>
     </Tabs>
   );
 }
